85fad5691328c724bb8716eecb9f56fd12933745
kyainuma <yasu6491@icloud.com>
2023-03-27 21:39:19 +0900

    For

--- main.go
+++ main.go
@@ -4,13 +4,10 @@ import (
 	"fmt"
 )
 
-func split(sum int) (x, y int) {
-	x = sum * 4 / 9
-	y = sum - x
-	return
-}
-
 func main() {
-	x, y := split(17)
-	fmt.Println(x, y)
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
 }
