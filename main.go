6a5088850536b1d02eaa9d69eb2468ec4978314c
kyainuma <yasu6491@icloud.com>
2023-03-27 21:47:35 +0900

    If

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func main() {
-	sum := 1
-	for sum < 1000 {
-		sum += sum
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
 	}
-	fmt.Println(sum)
+	return fmt.Sprint(math.Sqrt(x))
+}
+
+func main() {
+	fmt.Println(sqrt(2), sqrt(-4))
 }
