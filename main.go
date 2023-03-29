2521b81745186f3996cbe2f76d524862476b3efc
kyainuma <yasu6491@icloud.com>
2023-03-29 22:18:47 +0900

    Interface values with nil underlying values

--- main.go
+++ main.go
@@ -10,13 +10,27 @@ type T struct {
 	S string
 }
 
-// このメソッドは、型TがインターフェースIを実装していることを意味します
-// しかし、そうであることを明示的に宣言する必要はありません。
-func (t T) M() {
+func (t *T) M() {
+	if t == nil {
+		fmt.Println("<nil>")
+		return
+	}
 	fmt.Println(t.S)
 }
 
 func main() {
-	var i I = T{"hello"}
+	var i I
+
+	var t *T
+	i = t
+	describe(i)
 	i.M()
+
+	i = &T{"hello"}
+	describe(i)
+	i.M()
+}
+
+func describe(i I) {
+	fmt.Printf("(%v, %T)\n", i, i)
 }
